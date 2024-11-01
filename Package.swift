7d2cec6ef9abbb3a9325d7bd8fd75928691d94ab
Thomas Bonk <thomas@meandmymac.de>
2024-11-01 13:23:42 +0100

    using main branch of CLua

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "https://github.com/SwiftyLua/CLua", from: "5.4.3"),
+    .package(url: "https://github.com/SwiftyLua/CLua", .branch("main")),
     .package(url: "https://github.com/Quick/Quick", from: "3.1.2"),
     .package(url: "https://github.com/Quick/Nimble", from: "9.0.0")
   ],
